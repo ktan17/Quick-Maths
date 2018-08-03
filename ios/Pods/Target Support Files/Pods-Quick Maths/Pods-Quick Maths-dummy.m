e70e92deb6b26976d4092d882a4610b2834a4302
Kevin Tan <Kevin.Tan@gpiglobal.com>
2018-08-03 14:50:58 -0500

    fixed git

--- ios/Pods/Target Support Files/Pods-Quick Maths/Pods-Quick Maths-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quick_Maths : NSObject
-@end
-@implementation PodsDummy_Pods_Quick_Maths
-@end
--- ios/Pods/Target Support Files/Pods-Quick MathsTests/Pods-Quick MathsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quick_MathsTests : NSObject
-@end
-@implementation PodsDummy_Pods_Quick_MathsTests
-@end
